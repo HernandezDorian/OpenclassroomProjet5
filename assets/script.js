94a13552cdfeb20c907c5eac1c755f5bbfba31dc
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-10-03 14:43:01 +0200

    Carroussel JS

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,110 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+var img = document.querySelector(".banner-img");
+let arrowleft = document.querySelector(".arrow_left");
+let arrowright = document.querySelector(".arrow_right");
+let slidenow = 0;
+let dot = [
+	document.getElementById("dot0"),
+	document.getElementById("dot1"),
+	document.getElementById("dot2"),
+	document.getElementById("dot3")
+];
+let p_banner = document.getElementById("p_banner");
+
+async function delay(t) {
+	return new Promise(resolve => setTimeout(resolve, t));
+  };  // Fonction de delay (Permet de mettre un délai dans une fonction async)
+
+  function dotclick(v){
+	slidenow = v;
+	img.src=slides[v].image;
+	p_banner.innerHTML = slides[v].tagLine;
+	console.log("dotclick")
+	for (let index = 0; index < dot.length; index++) {
+		if (index != v) {
+			dot[index].classList.remove("dot_selected");	
+		}  else {
+			dot[index].classList.add("dot_selected");
+		}}
+}; // Déclanche le défilement en changeant les dots
+
+function detectdot(){
+	for (let index = 0; index < dot.length; index++) {
+		dot[index].addEventListener("click", function(){
+			dotclick(dot.indexOf(this))
+	})
+}} // Ajoute un écouteur dévenement sur tous les dots du array
+
+async function autodefile(t){
+	
+	for (slidenow; slidenow < slides.length+1; slidenow++) {
+		if (slidenow == 4) {
+			slidenow = 0
+			console.log("if" + slidenow)		
+		}
+		
+		// slidenow = index
+		dotclick(slidenow);
+		// console.log("auto" + index)
+		// slidenow++
+		// img.src=slides[index].image;
+		
+		await delay(t);
+		
+	}
+}; // Fonction de défilementt automatique du carroussel 
+
+autodefile(5000); // Défilement automatique de l'image ici changer le chiffre pour régler le temps en ms (Actuellement 5 secondes donc 5 000ms)
+
+function rightArrow() {
+	if (slidenow < 3) {
+		// img.src=slides[slidenow+=1].image;	
+		dotclick(slidenow+=1);	
+	} else {
+		// img.src=slides[0].image;
+		dotclick(0);
+		// slidenow = 0	
+	}
+}; // Flèche de gauche
+
+function leftArrow() {
+	if (slidenow > 0) {
+		// img.src=slides[slidenow-=1].image;
+		dotclick(slidenow-=1);		
+	} else {
+		// img.src=slides[3].image;
+		// slidenow = 3	
+		dotclick(3);
+	}
+}; // Flèche de droite
+
+
+
+detectdot() 
+
+arrowleft.addEventListener("click", function(){
+	leftArrow()
+}); // Ecoute le clique de l'utilisateur sur la flèche de gauche
+
+arrowright.addEventListener("click", function(){
+	rightArrow()
+}); // Ecoute le clique de l'utilisateur sur la flèche de droite
+
