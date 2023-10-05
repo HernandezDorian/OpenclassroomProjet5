c52a28695fa3e26e6860be1ee7c3e25b24a35178
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2023-10-05 15:32:12 +0200

    Simplification du code
    Avec les conseils de mon mentor j'ai pu simplifier le code et le réduire à 75 lignes au lieu de 96

--- assets/script.js
+++ assets/script.js
@@ -1,96 +1,75 @@
 // Déclarations des variables globales : 
 const slides = [
 	{
-		"image":"assets/images/slideshow/slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "assets/images/slideshow/slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"assets/images/slideshow/slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "assets/images/slideshow/slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"assets/images/slideshow/slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "assets/images/slideshow/slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"assets/images/slideshow/slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "assets/images/slideshow/slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+const img = document.querySelector(".banner-img");
+const arrowleft = document.querySelector(".arrow_left");
+const arrowright = document.querySelector(".arrow_right");
+const dot = document.querySelectorAll('.dot')
+const p_banner = document.getElementById("p_banner");
 
-var img = document.querySelector(".banner-img");
-let arrowleft = document.querySelector(".arrow_left");
-let arrowright = document.querySelector(".arrow_right");
 let slidenow = 0;
-let dot = [
-	document.getElementById("dot0"),
-	document.getElementById("dot1"),
-	document.getElementById("dot2"),
-	document.getElementById("dot3")
-];
-let p_banner = document.getElementById("p_banner");
 
-async function delay(t) {
-	return new Promise(resolve => setTimeout(resolve, t));
-  };  // Fonction de delay (Permet de mettre un délai dans une fonction async)
-
-  function dotclick(v){
+function dotclick(v) {
 	slidenow = v;
-	img.src=slides[v].image;
+	img.src = slides[v].image;
 	p_banner.innerHTML = slides[v].tagLine;
-	for (let index = 0; index < dot.length; index++) {
+	dot.forEach((element, index) => {
 		if (index != v) {
-			dot[index].classList.remove("dot_selected");	
-		}  else {
-			dot[index].classList.add("dot_selected");
-		}}
-}; // Déclanche le défilement en changeant les dots
-
-async function autodefile(t){
-	
-	for (slidenow; slidenow < slides.length+1; slidenow++) {
-		if (slidenow == 4) {
-			slidenow = 0	
+			element.classList.remove("dot_selected");
+		} else {
+			element.classList.add("dot_selected");
 		}
-		dotclick(slidenow);
-		
-		await delay(t);
-		
-	}
-}; // Fonction de défilementt automatique du carroussel 
+	})
+} // Déclanche le défilement en changeant les dots
+
+function autodefile(t) {
+	setInterval(rightArrow, t);
+} // Fonction de défilementt automatique du carroussel 
 
 function rightArrow() {
 	if (slidenow < 3) {
-		dotclick(slidenow+=1);	
+		dotclick(++slidenow);
 	} else {
 		dotclick(0);
 	}
-}; // Flèche de gauche
+} // Flèche de gauche
 
 function leftArrow() {
 	if (slidenow > 0) {
-		dotclick(slidenow-=1);		
+		dotclick(--slidenow);
 	} else {
 		dotclick(3);
 	}
-}; // Flèche de droite
+} // Flèche de droite
 
-function detectdot(){
-	for (let index = 0; index < dot.length; index++) {
-		dot[index].addEventListener("click", function(){
-			dotclick(dot.indexOf(this))
+function detectdot() {
+	dot.forEach((element, index) => {
+		element.addEventListener("click", function () {
+			dotclick(index)
+		})
 	})
-}} // Ajoute un écouteur dévenement sur tous les dots du array via une boucle for
+} // Ajoute un écouteur dévenement sur tous les dots du array via une boucle for
 
 autodefile(5000); // Défilement automatique de l'image ici changer le chiffre pour régler le temps en ms (Actuellement 5 secondes donc 5 000ms)
 
 detectdot(); // Ici on appel les écouteurs d'évenement sur les points
 
-arrowleft.addEventListener("click", function(){
-	leftArrow()
-}); // Ecoute le clique de l'utilisateur sur la flèche de gauche
+arrowleft.addEventListener("click", leftArrow); // Ecoute le clique de l'utilisateur sur la flèche de gauche
 
-arrowright.addEventListener("click", function(){
-	rightArrow()
-}); // Ecoute le clique de l'utilisateur sur la flèche de droite
-
+arrowright.addEventListener("click", rightArrow); // Ecoute le clique de l'utilisateur sur la flèche de droite
